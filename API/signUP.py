63cb59fd76dad4925e48ddf14532fa92f90cf8aa
Krzysiu! <117594482+ProfKrzys@users.noreply.github.com>
2024-11-14 17:45:55 +0100

    Create signUP.py

---
+++ API/signUP.py
@@ -0,0 +1,14 @@
+import random, string, requests
+
+def signup():
+    name = ''.join(random.choices(string.ascii_lowercase + string.digits, k=8))
+    email = name + "@gmail.com"
+    password = ''.join(random.choices(string.ascii_lowercase + string.digits, k=8))
+
+    query = {"name":name, "email":email, "password":password}
+
+    response = requests.get("https://api.mineserwery.pl/account.register", params=query)
+    if response.status_code == 200 and response.json()["type"] == "success":
+        token = response.json()["token"]
+        return name, email, password, token
+    return False
