293ccdac74f4676995b2b523dbbe1d6d4e4c85d9
Krzysiu! <117594482+ProfKrzys@users.noreply.github.com>
2024-11-14 17:46:08 +0100

    Add files via upload

---
+++ API/comment.py
@@ -0,0 +1,16 @@
+import requests
+
+def comment(name, token, serverid, message, positive):
+    if positive == True:
+        positive = "true"
+    else:
+        positive = "false"
+
+    query = {"login":name,"token":token,"mark":positive,"text":message,"server":str(serverid)}
+
+    response = requests.get("https://api.mineserwery.pl/server.sendComment", params=query)
+    if response.status_code == 200 and response.json()["type"] == "success":
+        return True
+    elif response.status_code == 200 and response.json()["error"] == 14:
+        return "Comment to short"
+    return False
